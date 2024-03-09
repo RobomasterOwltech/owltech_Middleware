29277747fe34b8ac17ff5444766421aea70c2ef4
Jorge Perez <jorge.perez.chavez@oracle.com>
2024-03-09 12:56:13 -0600

    added initial structs

---
+++ GenericComponents/inc/Structs.cpp
@@ -0,0 +1,30 @@
+// This file contains the bas
+
+CAN_MSG_Struct
+* id
+* function_Code
+* data_2_send
+
+PWM_Msg_struct
+ frequency
+duty_cycle 
+prescaler
+* PinId
+* HAL_Timer
+
+Motor
+* CAN_Msg
+
+Direction
+* Actual_Status_Sign
+
+Union
+* Max_M
+
+Encoder_Feedback
+Resolution
+*HAL_timer
+*Pin1
+*Pin2
+velocity
+last_tick
