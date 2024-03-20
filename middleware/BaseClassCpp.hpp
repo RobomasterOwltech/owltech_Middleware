d0f81443347afa6080a9fc95a0aaf06fcbb0973e
Jorge Perez <jorge.perez.chavez@oracle.com>
2024-03-20 17:04:21 -0600

    BaseFiles

---
+++ middleware/BaseClassCpp.hpp
@@ -0,0 +1,29 @@
+/*
+ * MotorPI.h
+ *
+ *  Created on: May 24, 2023
+ *      Author: josue
+ */
+
+#ifndef BaseFileC
+#define BaseFileC
+
+class BaseClassCpp
+{
+private:
+    /* data */
+public:
+    BaseClassCpp(/* args */);
+    ~BaseClassCpp();
+};
+
+BaseClassCpp::BaseClassCpp(/* args */)
+{
+}
+
+BaseClassCpp::~BaseClassCpp()
+{
+}
+
+
+#endif /* BaseFileC */
---
+++ middleware/BaseFileC.hpp
@@ -0,0 +1,20 @@
+/*
+ * MotorPI.h
+ *
+ *  Created on: May 24, 2023
+ *      Author: josue
+ */
+
+#ifndef BaseFileC
+#define BaseFileC
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BaseFileC */
