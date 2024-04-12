dd63676c65dbec7633c7b68b0d348e9daf6d0a02
Jorge Perez <jorge.perez.chavez@oracle.com>
2024-04-12 15:16:16 -0600

    I think base motor is complete :)

--- owltechMiddleware/inc/BaseMotor.hpp
+++ owltechMiddleware/inc/BaseMotor.hpp
@@ -1,5 +1,5 @@
 /*
- * BaseMotor.h
+ * BaseMotor.hpp
  *
  *  Created on: Apr 10, 2024
  *      Author: @JorgePerC
@@ -10,30 +10,51 @@
 #include "ControllerCAN.hpp"
 #include "ControllerPWM.hpp"
 
-class BaseMotor
-{
-private:
-    // TODO: Should we use Typedef?
-    struct operationalRanges {
+typedef enum {
+    POS,
+    VEL,
+    TOR
+} OperationModes;
+
+typedef struct {
         float maxVelocity;
         float minVelocity;
         float maxTorque;
         float minTorque;
         float maxPosition;
         float minPosition;
-    } ;
+} OperationalRanges;
 
-    union controller
-    {
-        ControllerCAN canController;
-        ControllerPWM pwmController;
-    };
+typedef union {
+    ControllerCAN* can;
+    ControllerPWM* pwm;
+} Controller;
 
-    bool direction;
+class BaseMotor {
+private:
+    Controller contr;
+    OperationalRanges* attr;
+    
+    uint8_t mode;
+    uint8_t dir;
+    u_int16_t runFreq;
+
+    virtual void actuateVelocity();
+    virtual void actuatePosition();
+    virtual void actuateTorque();
     
 public:
-    BaseMotor(/* args */) {}
-    ~BaseMotor() {}
+    BaseMotor(ControllerCAN* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction);
+    BaseMotor(ControllerPWM* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction);
+    virtual float getFeedback();
+    void actuate();
+    // The input value is an angular velocity
+    virtual void setReference(float w);
+    virtual void setControlType(OperationModes mode);
+    void invert();
+    void stop();
+
+    ~BaseMotor();
 };
 
 
---
+++ owltechMiddleware/src/BaseMotor.cpp
@@ -0,0 +1,46 @@
+/*
+ * BaseMotor.cpp
+ *
+ *  Created on: Apr 12, 2024
+ *      Author: @JorgePerC
+ */
+#include "BaseMotor.hpp"
+
+BaseMotor::BaseMotor(ControllerCAN* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction){
+    // TODO: Check pointers
+    // TODO: validate parameters before successful obj creation
+    this->contr.can = controller;
+    this->attr = attr;
+    this->dir = direction;
+}
+
+BaseMotor::BaseMotor(ControllerPWM* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction){
+    this->contr.pwm = controller;
+    this->attr = attr;
+    this->dir = direction;
+}
+
+void BaseMotor::actuate(){
+    switch (mode){
+    case POS:
+        actuatePosition();
+        break;
+    case VEL:
+        actuateVelocity();
+        break;
+    case TOR:
+        actuateTorque();
+        break;
+    default:
+        //TODO: Throw an error
+        break;
+    }
+}
+
+void BaseMotor::invert(){
+    this->dir *= -1;
+}
+
+void BaseMotor::stop(){
+    this->setReference(0);
+}
