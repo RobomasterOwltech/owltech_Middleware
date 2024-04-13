17f04ab5e5676717680c0449d4e5acaaf12874a8
Jorge Perez <jorge.perez.chavez@oracle.com>
2024-04-13 15:19:11 -0600

    revision Jorge

--- owltechMiddleware/inc/ControllerPWM.hpp
+++ owltechMiddleware/inc/ControllerPWM.hpp
@@ -1,19 +1,22 @@
 #include "stm32f4xx_hal.h"
 
+        
+
 class ControllerPWM {
    private:
-    typedef struct {
-        uint16_t frequency;
-        uint16_t duty_cycle;
-        uint16_t prescaler;
-        TIM_HandleTypeDef* timer;
-        uint32_t channel;
-    } PWM_MSG_STR;
+    uint16_t clock_frequency;
+    uint16_t pwm_frequency;
+    uint16_t duty_cycle;
+    uint16_t prescaler;
+    TIM_HandleTypeDef* timer;
+    uint32_t channel;
 
     // Signal variables
     int maximum_frequency;
     int minimum_frequency;
-    int run_frequency;
+
+    void set_frequency(uint16_t _frequency);
+    void set_prescaler(uint16_t _prescaler);
 
    public:
     ControllerPWM(TIM_HandleTypeDef* _timer, int minFreq, int maxFreq);
@@ -22,11 +25,8 @@ class ControllerPWM {
     uint16_t get_frequency();
     uint16_t get_duty_cycle();
     uint16_t get_prescaler();
-    uint16_t get_pin_id();
 
-    void set_frequency(uint16_t _frequency);
     void set_duty_cycle(uint16_t _duty_cycle);
-    void set_prescaler(uint16_t _prescaler);
 
     virtual void set_positon(int desiredPosition);
     virtual void set_velocity(int desiredVelocity);
--- owltechMiddleware/src/ControllerPWM.cpp
+++ owltechMiddleware/src/ControllerPWM.cpp
@@ -1,22 +1,23 @@
 #include "ControllerPWM.hpp"
 
 ControllerPWM::ControllerPWM(TIM_HandleTypeDef* _timer, int minFreq, int maxFreq) {
-    PWM_MSG_STR.timer = _timer;
-    PWM_MSG_STR.duty_cycle = 0;
-    PWM_MSG_STR.frequency = 1;
-    PWM_MSG_STR.prescaler = 0;
-    run_frequency = 1;
+    timer = _timer;
+    clock_frequency = 1/(_timer->Init.Period*_timer->Init.Prescaler);
     minimum_frequency = minFreq;
     maximum_frequency = maxFreq;
 }
 
 ControllerPWM::~ControllerPWM() {}
-uint16_t ControllerPWM::get_frequency() { return PWM_MSG_STR.frequency; }
-uint16_t ControllerPWM::get_duty_cycle() { return PWM_MSG_STR.duty_cycle; }
-uint16_t ControllerPWM::get_prescaler() { return PWM_MSG_STR.prescaler; }
+uint16_t ControllerPWM::get_frequency() { return pwm_frequency; }
+uint16_t ControllerPWM::get_duty_cycle() { return duty_cycle; }
+uint16_t ControllerPWM::get_prescaler() { return prescaler; }
 
-void ControllerPWM::set_frequency(uint16_t _frequency) { PWM_MSG_STR.frequency = _frequency; }
-void ControllerPWM::set_duty_cycle(uint16_t _duty_cycle) { PWM_MSG_STR.duty_cycle = _duty_cycle; }
-void ControllerPWM::set_prescaler(uint16_t _prescaler) { PWM_MSG_STR.prescaler = _prescaler; }
+void ControllerPWM::set_frequency(uint16_t _frequency) { 
+    // TODO: Operacion
+    pwm_frequency = _frequency; }
+void ControllerPWM::set_duty_cycle(uint16_t _duty_cycle) { duty_cycle = _duty_cycle; }
+void ControllerPWM::set_prescaler(uint16_t _prescaler) { 
+    // Todo: operación
+    prescaler = _prescaler; }
 
 void ControllerPWM::calibrateSignal() {}
