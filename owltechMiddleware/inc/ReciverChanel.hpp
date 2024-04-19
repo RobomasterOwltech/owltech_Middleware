840a2665cbeb62aa756a5242346daa6ce544f8a4
carloseag1234 <102176867+carloseag1234@users.noreply.github.com>
2024-04-19 16:03:31 -0600

    PWM almost working

--- owltechMiddleware/inc/ReciverChanel.hpp
+++ owltechMiddleware/inc/ReciverChanel.hpp
@@ -10,15 +10,20 @@
 #define ReciverChanel
 
 class ReciverChanel
+
+// ===== Includes =====
+#include "stm32f4xx_hal.h"
+
 {
 private:
-    TIM_HandleTypeDef* htim_;
-    uint32_t channel_;
-    uint32_t pulse_width_;
+    TIM_HandleTypeDef* htim;
+    uint32_t channel1;
+    uint32_t channel2;
+    uint32_t pulse_width;
 
     static void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
 public:
-    ReciverChanel(TIM_HandleTypeDef* htim, uint32_t channel);
+    ReciverChanel(TIM_HandleTypeDef* htim, uint32_t channel1, uint32_t channel2);
     void begin();
     uint32_t getPWM();
     ~ReciverChanel();
--- owltechMiddleware/src/ReciverChanel.cpp
+++ owltechMiddleware/src/ReciverChanel.cpp
@@ -7,19 +7,25 @@
 
  #include "ReciverChanel.hpp"
 
-ReciverChanel::ReciverChanel(TIM_HandleTypeDef* htim, uint32_t channel) :
-    htim_(htim), channel_(channel), pulse_width_(0) {}
+ReciverChanel::ReciverChanel(TIM_HandleTypeDef* htim_, uint32_t channel1_, uint32_t channel2_) :
+    htim(htim), channel1(channel1_), channel2(channel2_), pulse_width(0) {}
 
 void ReciverChanel::begin() {
-    HAL_TIM_IC_Start_IT(htim_, channel_);
+    HAL_TIM_IC_Start_IT(htim, channel1);
+    HAL_TIM_IC_Start_IT(htim, channel2);
 }
 
 uint32_t ReciverChanel::getPulseWidth() {
-    return pulse_width_;
+    return pulse_width;
 }
 
 void ReciverChanel::HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
     if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
-        pulse_width_ = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+        capturedValue = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+        if(capturedValue){
+            frecuency = systemCoreCloclk / capturedValue;
+            pulse_width = 1000* HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2)/capturedValue;
+            
+        }
     }
 }
